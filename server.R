4d29df71c74ca6a43c4e1b1db654973cf12b5d70
Daniel Viorato <daniel.viorato@hotmail.com>
2021-11-26 16:35:45 -0600

    Change 26/11/2021 office

--- server.R
+++ server.R
@@ -1,113 +1,121 @@
 source("R-scripts/load-data.R")
 
-#Pat: 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
+#Pat: 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
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    
-    #First Graph filters
-    output$periodoUI <-renderUI({
-        sliderInput(
-            "periodoSer", "Periodo",
-            min = min(hurdat$fecha), 
-            max = max(hurdat$fecha),
-            value = c(min(hurdat$fecha), 
-                      max(hurdat$fecha)),
-            timeFormat = "%d-%m-%Y"
-        )
-    })
-    
-    output$fechaUI <-renderUI({
-        dateRangeInput(
-            inputId = "fechaSer", 
-            label = "Periodo",
-            start = min(hurdat$fecha),
-            end = max(hurdat$fecha),
-            min = min(hurdat$fecha), 
-            max = max(hurdat$fecha),
-            format = "dd-mm-yyyy",
-            separator = " - "
-        )
-    })
-    
-    observeEvent(input$switchUI,{
-        updateDateRangeInput(inputId = "fechaSer",
-                             start = input$periodoSer[[1]],
-                             end = input$periodoSer[[2]])
-    })
-    
-    observeEvent(input$fechaSer, {
-            updateSliderInput(inputId = "periodoSer",
-                              value = c(input$fechaSer[[1]], 
-                                        input$fechaSer[[2]]),
-                              timeFormat = "%d-%m-%Y")
+shinyServer(
+    function(input, output) {
+        #First Graph filters
+        output$periodoUI <-renderUI({
+            sliderInput(
+                inputId = "periodoSer", 
+                label = "Periodo",
+                min = min(hurdat$fecha), 
+                max = max(hurdat$fecha),
+                value = c(min(hurdat$fecha), 
+                          max(hurdat$fecha)),
+                timeFormat = "%d-%m-%Y"
+            )
         })
-    
-    output$statusUI <- renderUI({
-        i <- pickerInput(
-            "statusSer", label = "Status", 
-            choices = unique(estatusn), 
-            selected = unique(estatusn),
-            width = "100%",
-            options = list(`actions-box` = TRUE,
-                           `deselect-all-text` = "Quitar selección",
-                           `select-all-text` = "Seleccionar todo",
-                           `none-selected-text` = "Ninguno"), 
-            multiple = T
-        )
-        return(i)
-    })
-
-    output$distTotal <- renderPlot({ #Frequency histogram
-        hurdat %>%
-            filter(
-                year >= min(format(input$periodoSer)),
-                year <= max(format(input$periodoSer)),
-                estatus %in% estatusc[match(input$statusSer, estatusn)]
-            ) %>%
-            ggplot(aes(year, fill = estatus)) +
-            geom_histogram(binwidth = 1) +
-            scale_fill_viridis(discrete = T)+
-            theme_minimal()
-    })
-    
-    #Second graph filtered base
-    base_point <- reactive({
-        (
+        
+        output$fechaUI <-renderUI({
+            dateRangeInput(
+                inputId = "fechaSer", 
+                label = "Periodo",
+                start = min(hurdat$fecha),
+                end = max(hurdat$fecha),
+                min = min(hurdat$fecha), 
+                max = max(hurdat$fecha),
+                format = "dd-mm-yyyy",
+                separator = " - "
+            )
+        })
+        
+        observeEvent(
+            input$switchUI,{
+                updateDateRangeInput(
+                    inputId = "fechaSer",
+                    start = input$periodoSer[[1]],
+                    end = input$periodoSer[[2]])
+        })
+        
+        observeEvent(
+            input$fechaSer, {
+                updateSliderInput(
+                    inputId = "periodoSer",
+                    value = c(input$fechaSer[[1]],
+                              input$fechaSer[[2]]),
+                    timeFormat = "%d-%m-%Y")
+        })
+        
+        output$statusUI <- renderUI({
+            i <- pickerInput(
+                inputId = "statusSer", 
+                label = "Status", 
+                choices = unique(estatusn), 
+                selected = unique(estatusn),
+                width = "100%",
+                options = list(
+                    `actions-box` = TRUE,
+                    `deselect-all-text` = "Quitar selección",
+                    `select-all-text` = "Seleccionar todo",
+                    `none-selected-text` = "Ninguno"),
+                multiple = T
+            )
+            return(i)
+        })
+        
+        output$distTotal <- renderPlot({ #Frequency histogram
+            hurdat %>%
+                filter(
+                    year >= min(format(input$periodoSer)),
+                    year <= max(format(input$periodoSer)),
+                    estatus %in% estatusc[match(input$statusSer, estatusn)]
+                ) %>%
+                ggplot(aes(year, fill = estatus)) +
+                geom_histogram(binwidth = 1) +
+                scale_fill_viridis(discrete = T)+
+                theme_minimal()
+        })
+        
+        #Second graph filtered base
+        base_point <- reactive({
             if(input$statusvientoSer !="All"){
-                norder <- c(input$statusvientoSer, estatusc[-grep(input$statusvientoSer, estatusc)])
+                norder <- c(input$statusvientoSer, 
+                            estatusc[-grep(input$statusvientoSer, estatusc)])
                 rang <- 9:1} else{
                     norder <- estatusc
                     rang <- 1:9}
-            )
-             
-        a <- hurdat %>% 
-            filter(!is.na(min_presion))%>%
-            group_by(clave) %>%
-            summarise(max_nudos_viento = mean(max_nudos_viento),
-                      min_presion = mean(min_presion),
-                      estatus = factor(names(which.max(table(estatus))),
-                                       levels = norder),
-                      balpha = ifelse(input$statusvientoSer == "All", T,
-                                      ifelse(estatus == input$statusvientoSer,
-                                             T,F)), .groups = "drop") 
-        a <- a[unlist(sapply(rang, function(x) which(a$estatus == norder[x]))),]
-        return(a)
-    })
-    
-    output$compvipe <- renderPlot( #Point graph
-        base_point() %>%
-            ggplot(aes(x = max_nudos_viento, y = min_presion, 
-                       color = estatus, alpha = balpha, 
-                       size = balpha))+
-            geom_point(shape = 16) +
-            theme_minimal()+
-            scale_alpha_ordinal(range = if(input$statusvientoSer == "All"){
-                c(0.55, 0.55)} else{
-                    c(0.15, 1)})+
-            scale_size_ordinal(range = c(2,2.5))+
-            scale_color_manual(values=viridis(9),breaks = estatusc)
-    )
-})
-#https://stackoverflow.com/questions/63414423/how-to-connect-daterangeinput-and-sliderinput-in-r-shiny
+            
+            a <- hurdat %>% 
+                filter(!is.na(min_presion))%>%
+                group_by(clave) %>%
+                summarise(
+                    max_nudos_viento = mean(max_nudos_viento),
+                    min_presion = mean(min_presion),
+                    estatus = factor(names(which.max(table(estatus))),
+                                     levels = norder),
+                    balpha = ifelse(input$statusvientoSer == "All", T,
+                                    ifelse(estatus == input$statusvientoSer,
+                                           T,F)), .groups = "drop")
+            a <- a[unlist(sapply(rang, 
+                                 function(x) which(a$estatus == norder[x]))),]
+            return(a)
+        })
+        
+        output$compvipe <- renderPlot( #Point graph
+            base_point() %>%
+                ggplot(aes(x = max_nudos_viento, y = min_presion, 
+                           color = estatus, alpha = balpha, 
+                           size = balpha))+
+                geom_point(shape = 16) +
+                theme_minimal()+
+                scale_alpha_ordinal(
+                    range = if(input$statusvientoSer == "All"){
+                        c(0.55, 0.55)} else{
+                        c(0.15, 1)})+
+                scale_size_ordinal(range = c(2,2.5))+
+                scale_color_manual(values=viridis(9),breaks = estatusc)
+        )
+    }
+)
--- ui.R
+++ ui.R
@@ -1,45 +1,49 @@
 source("R-scripts/load-data.R")
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-    # Application title
-    titlePanel("Base de datos de huracanes: HURDAT2"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            materialSwitch("switchUI", 
-                           value = T),
-            conditionalPanel(#Investigar conditional panel
-                condition = "input.switchUI",
-                uiOutput("periodoUI")
-            ),
-            conditionalPanel(
-                condition = "!input.switchUI",
-                uiOutput("fechaUI")
-            ),
-            uiOutput("statusUI")
-        ,width = 4),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distTotal")
+shinyUI(
+    fluidPage(
+        # Application title
+        titlePanel("Base de datos de huracanes: HURDAT2"),
+        
+        # Sidebar with a slider input for number of bins
+        sidebarLayout(
+            sidebarPanel(
+                materialSwitch(
+                    inputId = "switchUI",
+                    value = T),
+                conditionalPanel(#Investigar conditional panel
+                    condition = "input.switchUI",
+                    uiOutput("periodoUI")
+                ),
+                conditionalPanel(
+                    condition = "!input.switchUI",
+                    uiOutput("fechaUI")
+                ),
+                uiOutput("statusUI")
+                ,width = 4),
+            
+            # Show a plot of the generated distribution
+            mainPanel(
+                plotOutput("distTotal")
             )
         ),
-    sidebarLayout(
-        sidebarPanel(
-            sliderTextInput(
-                "statusvientoSer", label = "Status",
-                choices = c("All", estatusc), selected = "All" 
+        sidebarLayout(
+            sidebarPanel(
+                sliderTextInput(
+                    inputId = "statusvientoSer", 
+                    label = "Status",
+                    choices = c("All", estatusc), 
+                    selected = "All" 
+                )
+            ),
+            mainPanel(
+                plotOutput("compvipe")
             )
         ),
-        mainPanel(
-            plotOutput("compvipe")
+        fluidRow(
+            #        verbatimTextOutput("prueba"),
+            #        tableOutput("prueba2")
         )
-    ),
-    fluidRow(
-#        verbatimTextOutput("prueba"),
-#        tableOutput("prueba2")
     )
-    )
-    )
+)
